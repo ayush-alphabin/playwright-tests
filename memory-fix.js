7f619e18a83094654f016cfe717654a33414df8e
Ayush Mania <ayushm@testdino.com>
2026-01-30 15:46:59 +0530

    Remove memory-fix.js file

--- memory-fix.js
+++
@@ -1,6 +0,0 @@
-// Memory leak fix
-function cleanupEventListeners() {
-  // Remove all event listeners on cleanup
-  window.removeEventListener('resize', handleResize);
-  document.removeEventListener('click', handleClick);
-}
