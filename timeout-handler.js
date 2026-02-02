33178b44fba8dcebb83c15297a73127ca91d6d20
Ayush Mania <ayushm@testdino.com>
2026-02-02 16:03:21 +0530

    Fix timeout issues with proper handler

---
+++ timeout-handler.js
@@ -0,0 +1,21 @@
+// Timeout handler utility
+class TimeoutHandler {
+  constructor(defaultTimeout = 5000) {
+    this.defaultTimeout = defaultTimeout;
+  }
+  
+  async withTimeout(promise, timeout = this.defaultTimeout) {
+    return Promise.race([
+      promise,
+      new Promise((_, reject) => 
+        setTimeout(() => reject(new Error('Operation timed out')), timeout)
+      )
+    ]);
+  }
+  
+  setDefaultTimeout(timeout) {
+    this.defaultTimeout = timeout;
+  }
+}
+
+module.exports = TimeoutHandler;
