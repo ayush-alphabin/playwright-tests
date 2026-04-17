31804cdaa1e9b4076aec67e00ccafbefc6eecafc
Ayush Mania <ayushm@testdino.com>
2026-04-17 10:54:37 +0530

    chore: Update package dependencies and Playwright configuration
    - Add local dependency for tdpw in package.json
    - Update Playwright version in GitHub Actions workflow to v1.58.2
    - Modify Playwright reporter configuration to use list, html, and json formats, while commenting out the previous @testdino/playwright configuration
    - Update package-lock.json with new dependencies and integrity hashes

--- playwright.config.js
+++ playwright.config.js
@@ -10,12 +10,15 @@ module.exports = defineConfig({
   timeout: process.env.CI ? 10000 : 5000,
   workers: process.env.CI ? 3 : 5,
   reporter: [
-    ['@testdino/playwright', {
-      serverUrl: 'https://staging-api.testdino.com',
-      token: 'trx_staging_4aa7ba96f02c8c0339159af527d7380f5a9ae051258358495bd71cc0bc1d7b37',
-      debug: true,
-      artifacts: false
-    }]
+    ['list'],
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+    ['json', { outputFile: './playwright-report/report.json' }],
+    // ['@testdino/playwright', {
+    //   serverUrl: 'https://staging-api.testdino.com',
+    //   token: 'trx_staging_4aa7ba96f02c8c0339159af527d7380f5a9ae051258358495bd71cc0bc1d7b37',
+    //   debug: true,
+    //   artifacts: false
+    // }]
   ],
   use: {
     trace: "retry-with-trace",
